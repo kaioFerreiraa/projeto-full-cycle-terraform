7762b5fa6edde3104df731b0be2a2c0dcc417d1f
kaioFerreiraa <kaiohenrique099@gmail.com>
2026-03-17 23:01:49 -0300

    creating subnet

--- vpc.tf
+++ vpc.tf
@@ -4,3 +4,23 @@ resource "aws_vpc" "new-vpc" {
     Name = "fullcycle-vpc"
   }
 }
+
+data "aws_availability_zones" "available" {}
+
+resource "aws_subnet" "new-subnet-1" {
+  vpc_id            = aws_vpc.new-vpc.id
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = data.aws_availability_zones.available.names[0]
+  tags = {
+    Name = "fullcycle-subnet-1"
+  }
+}
+
+resource "aws_subnet" "new-subnet-2" {
+  vpc_id            = aws_vpc.new-vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = data.aws_availability_zones.available.names[1]
+  tags = {
+    Name = "fullcycle-subnet-2"
+  }
+}
