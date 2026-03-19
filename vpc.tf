53949c288960fe999cd3faa7af5271b71cb65f75
kaioFerreiraa <kaiohenrique099@gmail.com>
2026-03-18 22:42:19 -0300

    creating security group

--- vpc.tf
+++ vpc.tf
@@ -17,3 +17,27 @@ resource "aws_subnet" "subnets" {
     Name = "${var.prefix}-subnet-${count.index + 1}"
   }
 }
+
+resource "aws_internet_gateway" "new_igw" {
+  vpc_id = aws_vpc.new-vpc.id
+  tags = {
+    Name = "${var.prefix}-igw"
+  }
+}
+
+resource "aws_route_table" "new_rtb" {
+  vpc_id = aws_vpc.new-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.new_igw.id
+  }
+  tags = {
+    Name = "${var.prefix}-rtb"
+  }
+}
+
+resource "aws_route_table_association" "new_rtb_assoc" {
+  count          = 2
+  subnet_id      = aws_subnet.subnets.*.id[count.index]
+  route_table_id = aws_route_table.new_rtb.id
+}
